a394c6726617664c094c60d29ff80f551516c9bc
flgonzaga <contato@fabiogonzaga.com.br>
2016-04-23 16:22:30 -0300

    Added files via upload

---
+++ populate-select.js
@@ -0,0 +1,20 @@
+/* 
+ * Author: Fabio Gonzaga
+ * https://github.com/flgonzaga/
+ */
+
+(function ($) {
+
+    $('select[data-source]').each(function () {
+        $this = $(this);
+        var remoteJSON = $this.attr('data-source');
+        $.getJSON(remoteJSON, function (data) {
+            $.each(data, function (optionValue, optionText) {
+                var selected = ($this.attr('data-selected') == optionValue) ? 'selected' : '';
+                $this.append('<option value="' + optionValue + '" ' + selected + '>' + optionText + '</option>');
+            });
+        });
+
+    });
+
+})(jQuery);

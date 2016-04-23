a455d70531823a93b74447b9ad6fa03e6481b675
flgonzaga <contato@fabiogonzaga.com.br>
2016-04-23 18:42:26 -0300

    Update
    update to work multiple selects in same page

--- populate-select.js
+++ populate-select.js
@@ -6,7 +6,7 @@
 (function ($) {
 
     $('select[data-source]').each(function () {
-        $this = $(this);
+        var $this = $(this);
         var remoteJSON = $this.attr('data-source');
         $.getJSON(remoteJSON, function (data) {
             $.each(data, function (optionValue, optionText) {
@@ -14,7 +14,6 @@
                 $this.append('<option value="' + optionValue + '" ' + selected + '>' + optionText + '</option>');
             });
         });
-
     });
 
 })(jQuery);
